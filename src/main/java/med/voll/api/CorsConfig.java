ba9316a06bfb62145ac01557219548ffedf40961
luanAfons0 <luan.hmatao@gmail.com>
2025-01-19 22:09:07 -0300

    feat: added cors configuration

---
+++ src/main/java/med/voll/api/CorsConfig.java
@@ -0,0 +1,15 @@
+package med.voll.api;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PATCH","PUT","DELETE");
+    }
+}
